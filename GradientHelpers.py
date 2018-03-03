Parmameter calibration, midpoint calculation corrected, code cleaned

GradientHelpers.py
@@ -49,23 +49,29 @@ def dir_threshold(gray, sobel_kernel=3, thresh=(0, np.pi/2)):
     
     return mask
 
-def color_segmentation(img, l_thresh=[30, 255], s_thresh=[90, 255]):
+def color_segmentation(img, white_seg={'l_thres': [170,255], 's_thres': [0, 60], 'xmin': 680},
+                            yellow_seg={'l_thres': [20,200], 'h_thres': [16,23], 's_thres': [60, 255], 'xmax': 640},  clahe=False):
     img = np.copy(img)
+
     # Convert to HLS color space and separate the V channel
-    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS) #.astype(np.float)
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
     h_channel = hls[:,:,0]
     l_channel = hls[:,:,1]
     s_channel = hls[:,:,2]
     
-    #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
-    #l_channel = clahe.apply(l_channel)
-    l_thresh = [170,255]
-    s_thresh = [0, 60]
+    if clahe:
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
+        l_channel = clahe.apply(l_channel)
+        
     # Threshold color channel
-    white = (s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])
-    white &= (l_channel >= l_thresh[0]) & (l_channel <= l_thresh[1])
-    yellow = (h_channel >= 16) & (h_channel <= 23) & (s_channel >= 60)
-    yellow &= (l_channel >= 20) & (l_channel <= 200)
+    white = (s_channel >= white_seg['s_thres'][0]) & (s_channel <= white_seg['s_thres'][1])
+    white &= (l_channel >= white_seg['l_thres'][0]) & (l_channel <= white_seg['l_thres'][1])
+    white[:, 0:white_seg['xmin']] = False
+
+    yellow = (h_channel >= yellow_seg['h_thres'][0]) & (h_channel <= yellow_seg['h_thres'][1]) \
+                 & (s_channel >= yellow_seg['s_thres'][0])
+    yellow &= (l_channel >= yellow_seg['l_thres'][0]) & (l_channel <= yellow_seg['l_thres'][1])
+    yellow[:, yellow_seg['xmax']:] = False
     
     s_binary = white | yellow
 

LaneFit.py
@@ -11,6 +11,10 @@ class LaneFit():
         self.fit = False
         self.middle = 0.0
         self.cnt = 0
+        self.s1 = None
+        self.POLYFIT = True
+        self.PLOTFITPOINTS = False
+        self.POLYUPDATE = False
 
     def procVideoImg(self, img, leftx_base=None, rightx_base=None, numwin=9, margin=100, minpix=50):
         if self.firstCall:
@@ -23,39 +27,40 @@ class LaneFit():
 
         return self.left_fit, self.right_fit, window_img, left_curverad, right_curverad, self.middle
 
+    
     def update(self, img):
         
         # Identify the x and y positions of all nonzero pixels in the image
-        nonzero = img.nonzero()
+        nonzero = img[:,:,0].nonzero()
         nonzero_y = np.array(nonzero[0])
         nonzero_x = np.array(nonzero[1])
 
-        margin = 150
+        margin = 100
         if not self.firstCall:
-            left_lane  = self.left_fit[0]*(nonzero_y**2) + self.left_fit[1]*nonzero_y + self.left_fit[2]
-            right_lane = self.right_fit[0] * (nonzero_y**2) + self.right_fit[1] * nonzero_y + self.right_fit[2]
+            if self.POLYFIT:
+                if self.POLYUPDATE:
+                    # shift of curve while car is driving one frame
+                    delta_drive_px = 20
+                    self.left_fit[1] += 2. * delta_drive_px * self.left_fit[0]
+                    self.left_fit[2] += self.left_fit[0] * delta_drive_px**2 + delta_drive_px + self.left_fit[1]
+
+                    self.right_fit[1] += 2. * delta_drive_px * self.right_fit[0]
+                    self.right_fit[2] += self.right_fit[0] * delta_drive_px**2 + delta_drive_px + self.right_fit[1]
+                
+                # curve
+                delta_drive_px = 0
+                left_lane  = self.left_fit[0]*((nonzero_y+delta_drive_px)**2) + self.left_fit[1]*(nonzero_y+delta_drive_px) + self.left_fit[2]
+                right_lane = self.right_fit[0] * ((nonzero_y+delta_drive_px)**2) + self.right_fit[1] * (nonzero_y+delta_drive_px) + self.right_fit[2]
+
+            else:
+                delta_drive_px = 20
+
+                left_lane = self.s_left(nonzero_y+delta_drive_px)
+                right_lane = self.s_right(nonzero_y+delta_drive_px)
 
             left_lane_inds =  ((nonzero_x > left_lane - margin)) & (nonzero_x < left_lane + margin)
             right_lane_inds = ((nonzero_x > right_lane - margin)) & (nonzero_x < right_lane + margin)
-            mx = 50
-            if True:
-                if left_lane_inds.sum() > 50:
-                    window_mean = np.int(np.mean(nonzero_x[left_lane_inds]))
-                    # remove outliers
-                    mask = np.abs(nonzero_x - window_mean)
-                    left_lane_inds[mask > mx] = False
-
-                if right_lane_inds.sum() > 50:
-                    import math
-                    if math.isnan(np.mean(nonzero_x[right_lane_inds])):
-                        print(right_lane_inds)
-                        print(nonzero_x[right_lane_inds])
-                        print(np.mean(nonzero_x[right_lane_inds]))
-                    window_mean = np.int(np.mean(nonzero_x[right_lane_inds]))
-                    # remove outliers
-                    mask = np.abs(nonzero_x - window_mean)
-                    right_lane_inds[mask > mx] = False
-
+            
             # extract left and right line pixel positions
             leftx = nonzero_x[left_lane_inds]
             lefty = nonzero_y[left_lane_inds] 
@@ -66,8 +71,9 @@ class LaneFit():
 
             window_img = np.zeros_like(img)
 
-            #window_img[lefty, leftx] = [255, 255, 255]
-            #window_img[righty, rightx] = [255, 255, 255]
+            if self.PLOTFITPOINTS:
+                window_img[lefty, leftx] = [0, 0, 255]
+                window_img[righty, rightx] = [0, 0, 255]
 
             if self.fit:
                 # plot lane borders and area
@@ -84,14 +90,17 @@ class LaneFit():
                 right_curverad = 1
 
             return self.left_fit, self.right_fit, window_img, left_curverad, right_curverad, self.middle
-        
-    def fitCurves(self, leftx, lefty, rightx, righty, kr=9.):
+    
+    
+    def fitCurves(self, leftx, lefty, rightx, righty, kr=6):
         # Fit a second order polynomial to each
         # Define conversions in x and y from pixels space to meters
         ym_per_pix = 30/720 # meters per pixel in y dimension
         xm_per_pix = 3.7/700 # meters per pixel in x dimension
-        print(str(len(lefty))+'   '+str(len(righty)))
-        if len(lefty) > 10000 and len(righty) > 10000:
+        
+        #print(str(len(lefty))+'   '+str(len(righty)))
+        
+        if len(lefty) > 1000 and len(righty) > 1000:
             if self.cnt > 0:
                 self.cnt = self.cnt - 1
             else:       
@@ -107,14 +116,16 @@ class LaneFit():
                     poly_r = np.polyfit(righty, rightx, 2)
                     self.left_fit = (kr * self.left_fit + poly_l) / (kr + 1.)
                     self.right_fit = (kr * self.right_fit + poly_r) / (kr + 1.)
+                    
                     poly_l = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
                     poly_r = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
                     self.left_fit_phys = (kr * self.left_fit_phys + poly_l) / (kr + 1.)
                     self.right_fit_phys = (kr * self.right_fit_phys + poly_r) / (kr + 1.)
                 
                 # middle point
-                left_fitx = self.left_fit[2]
-                right_fitx = self.right_fit[2]            
+                bottom = 720
+                left_fitx  = self.left_fit[0]*(bottom**2) + self.left_fit[1]*bottom + self.left_fit[2]
+                right_fitx = self.right_fit[0] * (bottom**2) + self.right_fit[1] * bottom + self.right_fit[2]
                 self.middle = ((self.shape[1] / 2) - ((left_fitx + right_fitx) / 2)) * xm_per_pix
         else:
             self.cnt = self.cnt + 1
@@ -125,8 +136,12 @@ class LaneFit():
         ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
         
         # curve
-        left_fitx = self.left_fit[0]*ploty**2 + self.left_fit[1]*ploty + self.left_fit[2]
-        right_fitx = self.right_fit[0]*ploty**2 + self.right_fit[1]*ploty + self.right_fit[2]
+        if self.POLYFIT:
+            left_fitx = self.left_fit[0]*ploty**2 + self.left_fit[1]*ploty + self.left_fit[2]
+            right_fitx = self.right_fit[0]*ploty**2 + self.right_fit[1]*ploty + self.right_fit[2]
+        else:
+            left_fitx = self.s_left(ploty)
+            right_fitx = self.s_right(ploty)        
 
         left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
         left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, 
@@ -209,14 +224,17 @@ class LaneFit():
         # Concatenate the arrays of indices
         left_lane_inds = np.concatenate(left_lane_inds)
         right_lane_inds = np.concatenate(right_lane_inds)
+
         # Extract left and right line pixel positions
         leftx = nonzero_x[left_lane_inds]
         lefty = nonzero_y[left_lane_inds] 
         rightx = nonzero_x[right_lane_inds]
         righty = nonzero_y[right_lane_inds] 
 
+        # fit the curve
         self.fitCurves(leftx, lefty, rightx, righty)
 
+        # output image
         window_img = np.zeros_like(out_img)
         if self.fit:
             # plot lane borders and area

ProcessImage.py
@@ -8,18 +8,24 @@ import time
 import os.path
 import glob
 from CameraCalibration import CalibrateCamera
-from GradientHelpers import abs_sobel_thresh, mag_thresh, dir_threshold, color_segmentation, mask_region_of_interest, img2gray
+from GradientHelpers import abs_sobel_thresh, mag_thresh, dir_threshold, color_segmentation, \
+                            mask_region_of_interest, img2gray
 from LaneFit import LaneFit
 from moviepy.editor import VideoFileClip
 from time import time
 import logging
 
-class LaneDetect():
+class ProcessImage():
     def __init__(self):
         self.shape = None
         self.roi = None
         self.calCam = None
-        laneFit = None
+        self.laneFit = None
+        self.image_cnt = 0
+        self.DEBUG_IMAGE = False
+        self.DEBUG_IMAGE_FOLDER = 'project_video'
+        self.DEBUG_VIDEO = False
+        self.segmentation = 1
 
     def fit(self, shape, M, MInverse, roi=None, calCam=None):
         self.shape = shape
@@ -49,7 +55,7 @@ class LaneDetect():
 
         self.update = False
 
-    def test1(self, img):
+    def imageClahe(self, img):
         hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS) 
         l_channel = hls[:,:,1]
         
@@ -72,40 +78,56 @@ class LaneDetect():
 
     def process_image(self, img_orig):
         t0 = time()
+        self.image_cnt += 1
 
-        img = self.calCam.undistort(img_orig)   
+        img = self.calCam.undistort(img_orig)
+
+        if self.DEBUG_IMAGE:
+            img_savename = 'image{0:04d}.jpg'.format(self.image_cnt)            
+            cv2.imwrite(self.DEBUG_IMAGE_FOLDER + '/undist/'+img_savename, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
         img_undist = img
         ksize = 9
 
         gray = img2gray(img)
 
         # Apply each of the thresholding functions
-        mag_binary = mag_thresh(gray, sobel_kernel=ksize, mag_thresh=(20, 255))
-        dir_binary = dir_threshold(gray, sobel_kernel=ksize, thresh=(np.pi/4*0.9, np.pi/4*1.5))
+        if self.segmentation == 0:
+            mag_binary = mag_thresh(gray, sobel_kernel=ksize, mag_thresh=(20, 255))
+            dir_binary = dir_threshold(gray, sobel_kernel=ksize, thresh=(np.pi/4*0.9, np.pi/4*1.5))
+            color_seg = color_segmentation(img)
+
+            seg_img_raw = color_seg & mag_binary & dir_binary
         
-        color_seg = color_segmentation(img, l_thresh=[30, 255], s_thresh=[160, 255])
-        
-        kernel_size = 5
-        blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
-        #canny = cv2.Canny(blur_gray, 120, 200)
-        canny = cv2.Canny(blur_gray, 40, 80).astype(np.uint8) * 255
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-        #mask = cv2.erode(mask, kernel, iterations = 1)
-        canny = cv2.dilate(canny, kernel, iterations = 2)
-        
-        seg_img_raw = (color_seg & canny)
+        else:
+            color_seg = color_segmentation(img)
+            
+            kernel_size = 5
+            blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
+            canny = cv2.Canny(blur_gray, 40, 80).astype(np.uint8) * 255
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+            canny = cv2.dilate(canny, kernel, iterations = 3)
+            
+            seg_img_raw = (color_seg & canny)
         
         seg_img = seg_img_raw.astype(np.uint8) * 255
 
-        # mask image
-        seg_img, vertices = mask_region_of_interest(seg_img, self.roi)
-        seg_img = np.dstack((seg_img, seg_img, seg_img))
+        seg_img[670:,:] = 0
 
-        
+        # mask image
+        #seg_img, vertices = mask_region_of_interest(seg_img, self.roi)
+        seg_img = np.dstack((seg_img, seg_img, seg_img))
 
         #visualization = np.dstack((np.zeros(dir_binary.shape), (dir_binary & mag_binary), color_seg)).astype(np.uint8) * 255
         
+        # warp to birds eye perspective
         seg_img_warped = cv2.warpPerspective(seg_img, self.M, (seg_img.shape[1], seg_img.shape[0]), flags=cv2.INTER_LINEAR)
+        
+        # thresholding for interpolated pixels
+        seg_img_warped = (seg_img_warped > 100).astype(np.uint8) * 255
+
+        if self.DEBUG_IMAGE:
+            cv2.imwrite(self.DEBUG_IMAGE_FOLDER + '/seg_warped/'+img_savename, seg_img_warped)
         
         if self.update:
             left_fit, right_fit, lane_img, lc, rc, mid = self.laneFit.procVideoImg(seg_img_warped, margin=60, numwin=20)
@@ -121,25 +143,28 @@ class LaneDetect():
 
             self.update = True
 
+        if self.DEBUG_IMAGE:
+            comb = cv2.addWeighted(seg_img_warped, 0.5, lane_img, 0.8, 0)
+            cv2.imwrite(self.DEBUG_IMAGE_FOLDER + '/combined/'+img_savename, cv2.cvtColor(comb, cv2.COLOR_BGR2RGB))
+
         lane_img_unwarped = cv2.warpPerspective(lane_img, self.MInverse, (lane_img.shape[1], lane_img.shape[0]), flags=cv2.INTER_LINEAR)
         
-        if True:
-            img = cv2.addWeighted(img, 1, lane_img_unwarped, 0.7, 0)
-        else:
-            mask = (lane_img_unwarped[:,:,0] != 0) | (lane_img_unwarped[:,:,1] != 0) | (lane_img_unwarped[:,:,2] != 0)
-            img[mask] = 0 #lane_img_unwarped[mask]
-        
-            img = img + lane_img_unwarped# np.maximum(lane_img_unwarped, img)
+        # combine original image with detection
+        img = cv2.addWeighted(img, 1, lane_img_unwarped, 0.7, 0)
 
+        # write information to image
         img = self.writeInfo(img, lc, rc, mid)
         
-        t1 = time()
-        
+        # time measurement
+        t1 = time()        
         logging.info('process_image: runtime = ' + str(t1))
 
-        #img = np.dstack((np.zeros_like(seg_img_raw), canny, color_seg)).astype(np.uint8) * 255 #+ 0.4 * lane_img_unwarped
-        #img = cv2.addWeighted(img, 1, img_undist, 0.2, 0)
-        #img = cv2.addWeighted(img, 1, lane_img_unwarped, 0.9, 0)
+        if self.DEBUG_VIDEO:
+            img = np.dstack((np.zeros_like(seg_img_raw), canny, color_seg)).astype(np.uint8) * 255
+            img = cv2.addWeighted(img, 1, lane_img_unwarped, 0.9, 0)
+
+        if self.DEBUG_IMAGE:
+            cv2.imwrite(self.DEBUG_IMAGE_FOLDER + '/result/'+img_savename, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
         return img
 
@@ -148,7 +173,6 @@ def main():
 
     clip1 = VideoFileClip("source_videos/challenge_video.mp4").subclip(0,5)
 
-    original_bottom_left_x = 283
     target_left_x = 300
     target_right_x = 1002
     target_top_y = 0

RenderVideo.py
@@ -8,139 +8,17 @@ import time
 import os.path
 import glob
 from CameraCalibration import CalibrateCamera
-from GradientHelpers import abs_sobel_thresh, mag_thresh, dir_threshold, color_segmentation, mask_region_of_interest, img2gray
 from LaneFit import LaneFit
 from moviepy.editor import VideoFileClip
 from time import time
-from LaneDetect import LaneDetect
-
-if False:
-    calCam = CalibrateCamera.load()
-    img = mpimg.imread('test_images/straight_lines1.jpg')
-    # define region of interest
-    roi = { 'bottom_y':       img.shape[0],
-            'bottom_x_left':  int(img.shape[1]*0.05),
-            'bottom_x_right': int(img.shape[1]*0.95),
-            'top_y':          int(img.shape[0]*0.6),
-            'top_x_left':     int(img.shape[1]*0.45),
-            'top_x_right':    int(img.shape[1]*0.55),            
-            }
-    roi.update({'top_center': int((roi['top_x_left'] + roi['top_x_right']) / 2)})
-
-    horizon = 425
-
-    original_bottom_left_x = 283
-    target_left_x = 300
-    target_right_x = 1002
-    target_top_y = 0
-    target_bottom_y =690
-    src_points = np.float32([[283, 664], [548, 480], [736, 480],  [1019, 664]])
-    dst_points = np.float32([[target_left_x, target_bottom_y], [target_left_x, target_top_y],
-                                [target_right_x, target_top_y], [target_right_x, target_bottom_y]])
-
-    M = cv2.getPerspectiveTransform(src_points, dst_points)
-    Mi = cv2.getPerspectiveTransform(dst_points, src_points)
-
-    laneFit = LaneFit()
-    call = 0
-
-    def process_image(img_orig):
-        global call
-
-        t0 = time()
-
-        img = calCam.undistort(img_orig)   
-
-        ksize = 9
-
-        gray = img2gray(img)
-
-        if False:
-            hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS) 
-            l_channel = hls[:,:,1]
-            
-            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
-            l_channel = clahe.apply(l_channel)
-            hls[:,:,1] = l_channel
-
-            return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
-
-        # Apply each of the thresholding functions
-        mag_binary = mag_thresh(gray, sobel_kernel=ksize, mag_thresh=(20, 255))
-        dir_binary = dir_threshold(gray, sobel_kernel=ksize, thresh=(np.pi/4*0.9, np.pi/4*1.5))
-        
-        color_seg = color_segmentation(img, l_thresh=[30, 255], s_thresh=[160, 255])
-        #print(mag_binary.shape)
-        #print(dir_binary.shape)
-        #print(color_seg.shape)
-        #seg_img = (color_seg | (dir_binary & mag_binary)).astype(np.uint8) * 255
-        seg_img_raw = (color_seg & dir_binary & mag_binary)
-
-        seg_img = seg_img_raw.astype(np.uint8) * 255
-
-        # mask image
-        seg_img, vertices = mask_region_of_interest(seg_img, roi)
-
-        seg_img = np.dstack((seg_img, seg_img, seg_img))
-
-        #visualization = np.dstack((np.zeros(dir_binary.shape), (dir_binary & mag_binary), color_seg)).astype(np.uint8) * 255
-        
-        seg_img_warped = cv2.warpPerspective(seg_img, M, (seg_img.shape[1], seg_img.shape[0]), flags=cv2.INTER_LINEAR)
-
-        if call == 0:
-            histogram = np.sum(seg_img_warped[seg_img_warped.shape[0]//2:,:-2, 0], axis=0)
-
-            midpoint = np.int(histogram.shape[0]/2)
-            
-            leftx_base = np.argmax(histogram[:midpoint])
-            rightx_base = np.argmax(histogram[midpoint:]) + midpoint
-
-            left_fit, right_fit, lane_img, lc, rc = laneFit.fitLanes(seg_img_warped, leftx_base, rightx_base, margin=60, numwin=20)
-        else:
-            histogram = None
-            midpoint = None
-            leftx_base = None
-            rightx_base = None
-            left_fit, right_fit, lane_img, lc, rc = laneFit.fitLanes(seg_img_warped, leftx_base, rightx_base, margin=100, numwin=20)
-        #call = 1
-
-
-        lane_img_unwarped = cv2.warpPerspective(lane_img, Mi, (lane_img.shape[1], lane_img.shape[0]), flags=cv2.INTER_LINEAR)
-        
-        mask = (lane_img_unwarped[:,:,0] != 0) | (lane_img_unwarped[:,:,1] != 0) | (lane_img_unwarped[:,:,2] != 0)
-        img[mask] = 0 #lane_img_unwarped[mask]
-
-        img = img + lane_img_unwarped# np.maximum(lane_img_unwarped, img)
-
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        pos_str = 'Left curv:  {:06.0f}'.format(lc)
-        pos_str2 = 'Right curv: {:06.0f}'.format(rc)
-        #cv2.putText(img, pos_str ,(10,30), font, 0.5,(255,255,255), 1, cv2.LINE_AA)
-        #cv2.putText(img, pos_str2 ,(10,50), font, 0.5,(255,255,255), 1, cv2.LINE_AA)
-        
-        t1 = time()
-        #print(t1 - t0)
-
-        img = np.dstack((np.zeros_like(seg_img_raw), (dir_binary & mag_binary), color_seg)).astype(np.uint8) * 255 #+ 0.4 * lane_img_unwarped
-
-        return img
-
-    white_output = 'processed_videos/challenge_video.mp4'
-    ## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
-    ## To do so add .subclip(start_second,end_second) to the end of the line below
-    ## Where start_second and end_second are integer values representing the start and end of the subclip
-    ## You may also uncomment the following line for a subclip of the first 5 seconds
-    ##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
-    clip1 = VideoFileClip("source_videos/challenge_video.mp4")
-    white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
-    white_clip.write_videofile(white_output, audio=False)
+from ProcessImage import ProcessImage
 
 def main():
-    #white_output = 'processed_videos/challenge_video.mp4'
-    #clip1 = VideoFileClip("source_videos/challenge_video.mp4")#.subclip(0,5)
+    white_output = 'processed_videos/project_video.mp4'
+    clip1 = VideoFileClip("source_videos/project_video.mp4")#.subclip(0,5)
 
-    white_output = 'processed_videos/harder_challenge_video.mp4'
-    clip1 = VideoFileClip("source_videos/harder_challenge_video.mp4")#.subclip(0,5)
+    #white_output = 'processed_videos/harder_challenge_video.mp4'
+    #clip1 = VideoFileClip("source_videos/harder_challenge_video.mp4")#.subclip(0,5)
 
     original_bottom_left_x = 283
     target_left_x = 300
@@ -154,7 +32,7 @@ def main():
     M = cv2.getPerspectiveTransform(src_points, dst_points)
     Mi = cv2.getPerspectiveTransform(dst_points, src_points)
 
-    ld = LaneDetect()
+    ld = ProcessImage()
     ld.fit((720, 1280), M, Mi)
 
     white_clip = clip1.fl_image(ld.process_image) # color images